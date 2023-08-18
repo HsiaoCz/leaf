50d236c304ea644f08ebda6a1c0e1b65bceb1678
HsiaoCz <984274788@qq.com>
2023-08-18 17:15:50 +0800

    add config but not done

---
+++ dao/mysql/init.go
@@ -0,0 +1,9 @@
+package mysql
+
+import "gorm.io/gorm"
+
+var DB *gorm.DB
+
+func Init() {
+   
+}
--- etc/config.go
+++ etc/config.go
@@ -1,2 +1,83 @@
 package etc
 
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+var Conf = new(LeafConf)
+
+type LeafConf struct {
+	App   AppConf   `mapstructure:"app"`
+	Log   LogConf   `mapstructure:"log"`
+	Mysql MysqlConf `mapstructure:"mysql"`
+	Redis RedisConf `mapstructure:"redis"`
+	Mongo MongoConf `mapstructure:"mongo"`
+}
+
+type AppConf struct {
+	Name      string `mapstructure:"name"`
+	Mode      string `mapstructure:"dev"`
+	AppPort   int    `mapstructure:"app_port"`
+	Version   string `mapstructure:"version"`
+	StartTime string `mapstructure:"start_time"`
+	MachineID int    `mapstructure:"machine_id"`
+}
+
+type LogConf struct {
+	Level      string `mapstructure:"debug"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+type MysqlConf struct {
+	MysqlHost     string `mapstructure:"mysql_host"`
+	MysqlPort     int    `mapstructure:"mysql_port"`
+	User          string `mapstructure:"user"`
+	MysqlPassword string `mapstructure:"mysql_password"`
+	Dbname        string `mapstructure:"dbname"`
+	MaxOpenConns  string `mapstructure:"mx_open_conns"`
+	MaxIdleConns  string `mapstructure:"max_idle_conns"`
+}
+
+type RedisConf struct {
+	RedisHost     string `mapstructure:"redis_host"`
+	RedisPort     int    `mapstructure:"redis_port"`
+	DB            int    `mapstructure:"db"`
+	RedisPassword string `mapstructure:"redis_password"`
+	PoolSize      int    `mapstructure:"poolsize"`
+}
+
+type MongoConf struct {
+	MongoHost string `mapstructure:"mongo_host"`
+	MongoPort int    `mapstructure:"mongo_port"`
+	DbName    string `mapstructure:"db_name"`
+	Username  string `mapstructure:"username"`
+	Password  string `mapstructure:"password"`
+}
+
+func Init() (err error) {
+	viper.SetConfigName("conf")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./")
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	if err := viper.Unmarshal(Conf); err != nil {
+		return err
+	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		log.Println("配置文件修改了...")
+		if err := viper.Unmarshal(Conf); err != nil {
+			return
+		}
+	})
+	return err
+}
